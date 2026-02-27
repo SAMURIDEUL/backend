bfcf40e2ce0b8a223316d7cf06363a8ccab609a7
two678 <two6788@gmail.com>
2026-02-27 15:08:07 +0900

    fix: Swagger 배포 환경 HTTPS CORS 에러 대응 Server URL 명시

--- src/main/java/com/example/samuL/common/config/SwaggerConfig.java
+++ src/main/java/com/example/samuL/common/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.example.samuL.common.config;
 
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -10,15 +9,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI(){
+    public OpenAPI openAPI() {
         Info info = new Info()
                 .version("v1.0.0")
                 .title("Swagger 확인")
                 .description("Swagger 확인용 api 명세서 입니다.");
+
         return new OpenAPI()
+                .addServersItem(new io.swagger.v3.oas.models.servers.Server().url("/"))
                 .components(new Components())
                 .info(info);
     }
-
 
 }
